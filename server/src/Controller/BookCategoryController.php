c14cc1c00e224c9947cc98f7f2c530b626e5a554
VasiliyDR <vasilek96@bk.ru>
2024-03-15 20:58:20 +0300

    commmit

---
+++ server/src/Controller/BookCategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\BookCategoryListResponse;
+use App\Service\BookCategoryService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookCategoryController extends AbstractController
+{
+    public function __construct(
+        private readonly BookCategoryService $bookCategoryService
+    )
+    {
+    }
+
+    #[Route('api/v1/book/categories', methods: ['GET'])]
+    public function categories(): Response
+    {
+        return $this->json($this->bookCategoryService->getCategories());
+    }
+}
